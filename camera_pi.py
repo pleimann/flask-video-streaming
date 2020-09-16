682eb161583888437f9463a992603f478c75596b
Mike Pleimann <mike@pleimann.com>
2020-09-15 21:16:07 -0500

    Flip image because camera's upside down

--- camera_pi.py
+++ camera_pi.py
@@ -7,13 +7,17 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     @staticmethod
     def frames():
-        with picamera.PiCamera() as camera:
+        with picamera.PiCamera(resolution='1640x1232', framerate=10) as camera:
             # let camera warm up
             time.sleep(2)
 
+            camera.vflip = True
+            camera.hflip = True
+
             stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, 'jpeg',
-                                                 use_video_port=True):
+            for _ in camera.capture_continuous(stream, 
+                                                format='jpeg', 
+                                                use_video_port=True):
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
--- run-pi.sh
+++ run-pi.sh
@@ -1,1 +1,1 @@
-CAMERA=pi gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:5000 app:app
+CAMERA=pi gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:80 app:app
